bbac5dc04a1424b383b0996330aa32e5d27bd0ad
Najimkhon <qwerty1995.290@gmail.com>
2023-04-28 15:57:53 +0500

    adding constructors to layout class

--- app/src/main/java/ru/partyshaker/partyshaker/ui/TextInputCustomView.kt
+++ app/src/main/java/ru/partyshaker/partyshaker/ui/TextInputCustomView.kt
@@ -1,4 +1,19 @@
 package ru.partyshaker.partyshaker.ui
 
-class TextInputCustomView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class TextInputCustomView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+
 }
