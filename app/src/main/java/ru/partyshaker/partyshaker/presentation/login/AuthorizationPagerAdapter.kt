4587437704d4e09f1d0887bcfe2143f7bf583446
Najimkhon <qwerty1995.290@gmail.com>
2023-05-12 17:41:53 +0500

    Create AuthorizationPagerAdapter.kt

---
+++ app/src/main/java/ru/partyshaker/partyshaker/presentation/login/AuthorizationPagerAdapter.kt
@@ -0,0 +1,25 @@
+package ru.partyshaker.partyshaker.presentation.login
+
+import android.content.res.Resources
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import ru.partyshaker.partyshaker.ui.fragments.RegisterFragment
+
+class AuthorizationPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount() = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                LoginFragment()
+            }
+            1 -> {
+                RegisterFragment()
+            }
+            else -> {throw Resources.NotFoundException("Position Not Found")
+            }
+        }
+    }
+}
