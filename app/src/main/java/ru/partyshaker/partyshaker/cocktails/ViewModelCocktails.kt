ccdda3e6e407ac4f1fd555658f8719ca474546ea
Alena Moskovchuk <am79819712119@gmail.com>
2023-05-14 12:18:33 +0300

    created recycle view for cocktails (adapter, holder, view item and fragment)

--- app/src/main/java/ru/partyshaker/partyshaker/cocktails/ViewModelCocktails.kt
+++ app/src/main/java/ru/partyshaker/partyshaker/cocktails/ViewModelCocktails.kt
@@ -3,12 +3,13 @@ package ru.partyshaker.partyshaker.cocktails
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import ru.partyshaker.partyshaker.ApiResponse
 import javax.inject.Inject
 import ru.partyshaker.partyshaker.cocktails.data.data_classes.Result
 
-
+@HiltViewModel
 class ViewModelCocktails @Inject constructor(private val repository: RepositoryCocktails)  : ViewModel() {
 
     val cocktailsList = MutableLiveData<List<Result>?>()
