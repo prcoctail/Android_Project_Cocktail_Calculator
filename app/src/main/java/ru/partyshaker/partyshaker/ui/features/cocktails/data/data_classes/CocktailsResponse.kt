5feba2c85b752439d2ad01c3d14f8dbaa7c7afbb
Calloop <CalloopG@gmail.com>
2023-07-12 19:19:33 +0300

    Экран списка коктейлей fragment_cocktails.xml: доработан интерфейс, добавлена интерактивность, добавлено получение данных из API. Cocktails list screen fragment_cocktails.xml: improved interface, added interactivity, added data retrieval from API.

---
+++ app/src/main/java/ru/partyshaker/partyshaker/ui/features/cocktails/data/data_classes/CocktailsResponse.kt
@@ -0,0 +1,14 @@
+package ru.partyshaker.partyshaker.ui.features.cocktails.data.data_classes
+
+import com.google.gson.annotations.SerializedName
+
+data class CocktailsResponse(
+//    @SerializedName("count")
+//    val count: Int,
+//    @SerializedName("next")
+//    val next: String?,
+//    @SerializedName("previous")
+//    val previous: String?,
+    @SerializedName("results")
+    val results: List<Cocktail>
+)
